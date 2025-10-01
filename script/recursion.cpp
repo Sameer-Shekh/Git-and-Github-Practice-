9e094d3ef35e97511845a4f670ac34fedf332e55
Sameer Shekh <khansameer7311@gmail.com>
2025-10-01 17:37:44 +0530

    write printNumber with recursion

---
+++ script/recursion.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void printNumber(int n){
+
+    //BASE CASE
+    if(n<1){
+        return;
+    }
+
+    //PRINTING
+    cout<<n<<",";
+
+    //RECURSIVE CALL
+    printNumber(n-1);
+}
+
+int main()
+{
+    int n;
+    cout<<"ENTER NO"<<endl;
+    cin>>n;
+    printNumber(n);
+    return 0;
+}
